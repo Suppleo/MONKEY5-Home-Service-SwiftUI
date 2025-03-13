b4da214f1765065d6f77579ce0ac65cecdef0211
umtlab03im09 <umtimac08@umtlab03im09s-iMac.local>
2025-03-13 09:32:32 +0700

    Initial commit

---
+++ Home Service Management System/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Home Service Management System
+//
+//  Created by umtlab03im09 on 13/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Home Service Management System/Home_Service_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Service_Management_SystemApp.swift
+//  Home Service Management System
+//
+//  Created by umtlab03im09 on 13/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Home_Service_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
