50f0e02e28b7f702481bff0726bd65413080c0a2
Thach <ngocthachnguyen4399@gmail.com>
2025-03-13 10:14:37 +0700

    Add initial sample screens

---
+++ Home Service Management System/ProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileView.swift
+//  Home Service Management System
+//
+//  Created by umtlab03im09 on 13/3/25.
+//
+
+import Foundation
---
+++ Home Service Management System/ServiceDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceDetailView.swift
+//  Home Service Management System
+//
+//  Created by umtlab03im09 on 13/3/25.
+//
+
+import Foundation
